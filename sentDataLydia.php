a65c7e64a959a17456464e7a51d56b940d38155f
Lydia Neghad <101603670+Liily77@users.noreply.github.com>
2024-10-22 22:48:36 +0200

    Add files via upload

---
+++ sentDataLydia.php
@@ -0,0 +1,21 @@
+<?php  
+
+
+echo "<pre>";
+print_r($_POST);
+
+
+echo "</pre>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php //echo "<p>Bonjour ".$_POST['lastname']."</p>"; ?>	
+</body>
+</html>
